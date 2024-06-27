87d0b70e5fa359be6f55071f7cc36a032a295838
Pedro <pedro.005henr@gmail.com>
2024-06-26 22:22:39 -0300

    feat: object recognition webcam

---
+++ README.md
@@ -0,0 +1,15 @@
+# EasyVisionXR
+
+An application for you to learn about XR topics using Computational Vision.
+
+## Getting started
+
+...
+
+## Content
+
+...
+
+## How to contribute
+
+...
---
+++ object_recognition.py
@@ -0,0 +1,68 @@
+from ultralytics import YOLO
+import cv2
+from pathlib import Path
+
+RED = (255, 0, 0)
+
+
+def get_model_data():
+    return (YOLO("yolo-Weights/yolov8n.pt"),
+            ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+             "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+             "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+             "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+             "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+             "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+             "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+             "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+             "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+             "teddy bear", "hair drier", "toothbrush"
+             ])
+
+
+def get_font_data(x, y):
+    return [x, y], cv2.FONT_HERSHEY_DUPLEX, 2, RED, 2
+
+
+def webcam_recognition(webcam_model, webcam_classnames):
+    # Webcam and model setup
+    cap = cv2.VideoCapture(0)  # // if you have second camera you can set first parameter as 1
+    cap.set(3, 800)
+    cap.set(4, 600)
+
+    if not cap.isOpened():
+        print("Couldn't open camera")
+        exit(1)
+
+    while True:
+        success, img = cap.read()
+        recognitions = webcam_model(img, stream=True)
+
+        for r in recognitions:
+            boxes = r.boxes
+
+            for box in boxes:
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+                cv2.rectangle(img, (x1, y1), (x2, y2), RED, 2)
+
+                className = webcam_classnames[int(box.cls[0])]
+                pos, font, fontScale, fontColor, fontWeight = get_font_data(x1, y1)
+
+                cv2.putText(img, className, pos, font, fontScale, fontColor, fontWeight)
+
+        cv2.imshow('Webcam', img)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    exit(0)
+
+
+if __name__ == '__main__':
+    model, classNames = get_model_data()
+    webcam_recognition(model, classNames)
+    # image_recognition(mode, classNames, image)
+    # video_recognition(mode, classNames, video)
