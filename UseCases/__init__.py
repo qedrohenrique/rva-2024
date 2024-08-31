d6561caa4781a0acb54634fdb5f8691ec8276c6c
mtfs1 <math.santos.2002@gmail.com>
2024-08-31 16:19:41 -0300

    feat: train model and predict options in menu

--- UseCases/__init__.py
+++ UseCases/__init__.py
@@ -2,12 +2,15 @@ from UseCases.object_recognition import main as object_recognition
 from UseCases.object_tracking import main as object_tracking
 from UseCases.object_reconstruct import main as object_reconstruction
 from UseCases.image_segmentation import main as image_segmentation
+from UseCases.train_model import create_model, select_model
 
 
 options = {
     "object tracking": object_tracking,
     "object recognition": object_recognition,
     "object_reconstruction": object_reconstruction,
-    "image_segmentation": image_segmentation
+    "image_segmentation": image_segmentation,
+    "train model": create_model,
+    "predict": select_model,
 }
 
